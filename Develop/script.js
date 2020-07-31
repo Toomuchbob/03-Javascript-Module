cac4307d3669fbe8fc0573bf851a4c2310ae77a0
Gino Zaccardelli <ginozacc@gmail.com>
2020-07-30 21:25:21 -0400

    working version with comments, added generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //array of letters
 var letArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 //array of uppercase letters
 var upArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 //array of special characters
 var specArr = ["\!", "\"", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", 
-  "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\\", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"];
+"\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\[", "\\", "\]", "\^", "\_", "\`", "\{", "\|", "\}", "\~"];
 
 //prompt user to enter password length between 8-128
 var passwordLength = prompt("Please enter the desired length of the password \(between 8-128 characters\)");
@@ -55,17 +43,17 @@ if (pwdN) {
 if (pwdS) {
   userChoices.push('pwdS');
 }
-console.log(userChoices);
 
-//iterate through the chosen password length
-for (var i = 0; i < passwordLength; i++) {
-  //get next character type for password
-  var randomChoice = Math.floor(Math.random() * userChoices.length);
-  //find type and push to choices array
-  switch (userChoices[randomChoice]) {
-    case 'pwdL':
-      choice.push(letArr[Math.floor(Math.random() * letArr.length)]);
-      break;
+function generatePassword() {
+  //iterate through the chosen password length
+  for (var i = 0; i < passwordLength; i++) {
+    //get next character type for password
+    var randomChoice = Math.floor(Math.random() * userChoices.length);
+    //find type and push to choices array
+    switch (userChoices[randomChoice]) {
+      case 'pwdL':
+        choice.push(letArr[Math.floor(Math.random() * letArr.length)]);
+        break;
     case 'pwdU':
       choice.push(upArr[Math.floor(Math.random() * upArr.length)]);
       break;
@@ -73,11 +61,23 @@ for (var i = 0; i < passwordLength; i++) {
       choice.push(Math.floor(Math.random() * 10));
       break;
     case 'pwdS':
-      console.log(" special character incoming");
       choice.push(specArr[Math.floor(Math.random() * specArr.length)]);
       break;
     default:
       choice.push(Math.floor(Math.random() * 10));
       break;
+    }
   }
+  return choice.join('');
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
